011de1a1aac4eb4d790d8d3d98edbcac80cb6db6
bschilder <brian.schilder@mssm.edu>
2019-01-05 01:51:55 -0500

    Enable dynamic html output construction based on command line inputs

---
+++ seurat_params.R
@@ -0,0 +1,16 @@
+args = commandArgs(trailingOnly = T)
+
+subsetGenes = args[1]
+subGenes <- ifelse(subsetGenes==F, 'all', subsetGenes)
+subsetCells = args[2]
+resolution = args[3]
+parameters <- paste(sep='', 'subsetGenes-',subGenes,'__subsetCells-',subsetCells, '__Resolution-', resolution)
+root = getwd()
+resultsPath <- file.path(root,'Results',parameters)
+dir.create(resultsPath, showWarnings=F, recursive=T)
+        
+
+rmarkdown::render(input = 'run_seurat.Rmd', 
+                  params = list(subsetGenes= subsetGenes, subsetCells=subsetCells, resolution=resolution, resultsPath=resultsPath), 
+                  output_file = file.path(resultsPath,paste(parameters,".html",sep=""))
+                  )
