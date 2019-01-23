4bd88bf1be57e47fe148ee4db1f01f3a515450fc
bschilder <brian.schilder@mssm.edu>
2019-01-22 19:49:03 -0500

    Cluster/WGCNA module enrichment analyses; rename pbmc -> DAT; alter tSNE points alpha

--- seurat_params.R
+++ seurat_params.R
@@ -7,16 +7,20 @@ subGenes <- ifelse(subsetGenes==F, 'all', subsetGenes)
 subsetCells = args[2]
 resolution = args[3]
 nCores = args[4] 
+perplexity = args[5]
 
 #nCores <-args[4] #ifelse(is.null(args[4]), parallel::detectCores(), args[4]) 
 
-parameters <- paste(sep='', 'subsetGenes-',subGenes,'__subsetCells-',subsetCells, '__Resolution-', resolution, '__nCores-',nCores)
+parameters <- paste(sep='', 'subsetGenes-',subGenes,'__subsetCells-',subsetCells, 
+                    '__Resolution-', resolution,'__perplexity-',perplexity,
+                    '__nCores-',nCores)
 root = getwd()
 resultsPath <- file.path(root,'Results',parameters)
 dir.create(resultsPath, showWarnings=F, recursive=T)
         
 
 rmarkdown::render(input = 'run_seurat.Rmd', 
-                  params = list(subsetGenes= subsetGenes, subsetCells=subsetCells, resolution=resolution, resultsPath=resultsPath, nCores=nCores), 
+                  params = list(subsetGenes= subsetGenes, subsetCells=subsetCells, resolution=resolution, 
+                                resultsPath=resultsPath, nCores=nCores, perplexity=perplexity), 
                   output_file = file.path(resultsPath,paste(parameters,".html",sep=""))
                   )
