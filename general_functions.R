f76878ec775bad73034b8595af32899322d140d8
bschilder <brian.schilder@mssm.edu>
2019-02-21 00:21:21 -0500

    Within and between cluster DGE; functionalize gene overlap plots; fix createDT_html

--- general_functions.R
+++ general_functions.R
@@ -38,7 +38,7 @@ createDT <- function(DF, caption="", scrollY=500){
 }
 ## Need to wrap inside tagList when rendering DTs within a for loop
 createDT_html <- function(DF, caption="", scrollY=400){
-  print( htmltools::tagList( createDT(DF, caption, scrollY)) ) 
+  htmltools::tagList( createDT(DF, caption, scrollY)) 
 }
 
 # Raise Memory Limit 
@@ -53,15 +53,16 @@ raise_memory_limit <- function(){
   ## Set new memory limit 
   ulimit::memory_limit(RAM_Mib)  
 }
-
-
+ 
 volcanoPlot <- function(DEG_df, caption="", topFC_labeled=5){
   DEG_df$sig<-  ifelse( DEG_df$p_val_adj<0.05 & DEG_df$avg_logFC<1.5, "p_val_adj<0.05",
                         ifelse( DEG_df$p_val_adj<0.05  & DEG_df$avg_logFC>1.5, "p_val_adj<0.05 & avg_logFC>1.5",
                                 "p_val_adj>0.05"
                         )) 
   DEG_df <- arrange(DEG_df, desc(sig))
-  yMax  <- max(-log10(DEG_df$p_val_adj)) + max(-log10(DEG_df$p_val_adj))/3 #ifelse(max(-log10(DEG_df$p_val_adj))<45, 50, max(-log10(DEG_df$p_val_adj)) + 10)
+  neg.log.pvals <- -log10(DEG_df$p_val_adj) 
+  maximum <- max(neg.log.pvals[!is.infinite(neg.log.pvals)])
+  yMax  <- maximum + maximum/8 #ifelse(max(-log10(DEG_df$p_val_adj))<45, 50, max(-log10(DEG_df$p_val_adj)) + 10)
   
   vol <- ggplot(data=DEG_df, aes(x=avg_logFC, y= -log10(p_val_adj))) +
     geom_point(alpha=0.5, size=3, aes(col=sig)) + 
@@ -70,7 +71,8 @@ volcanoPlot <- function(DEG_df, caption="", topFC_labeled=5){
                                    "p_val_adj>0.05" = "darkgray")) +
     theme(legend.position = "none") + 
     xlab(expression(paste("Average ",log^{2},"(fold change)"))) +
-    ylab(expression(paste(-log^{10},"(p-value)"))) + xlim(-2,2) + ylim(0, yMax) +
+    ylab(expression(paste(-log^{10},"(p-value)"))) + 
+    xlim(-2,2) + ylim(0, yMax) +
     ## ggrepl labels
     geom_text_repel(data= arrange(DEG_df,  p_val_adj, desc(avg_logFC))[1:topFC_labeled,], 
                     # filter(DEG_df, avg_logFC>=1.5)[1:10,],
@@ -95,4 +97,41 @@ get_markerDT <- function(DAT, markerList, rawData=T, meta_vars =c("barcode", "dx
   markerMeta <- DAT@meta.data[meta_vars] %>% copy()  
   markerDT <- base::merge(markerMelt,markerMeta, by.x="Cell", by.y="barcode")  
   return(markerDT)
+}
+
+replace_zero_pvals <- function(DEGs){
+  # convert 0 to the smallest number R can represent(2.225074e-308)
+  DEGs[DEGs$p_val==0.0000000000000000000000000000,] <- 2.225074e-308  
+  DEGs[DEGs$p_val_adj==0.0000000000000000000000000000,] <- 2.225074e-308
+  return(DEGs)
+}
+
+runDGE <- function(DAT, meta_var, group1, group2, test.use="wilcox", show.table=T, show.volcano=T){
+  #print(paste("DGE_allCells",meta_var,sep="_")) 
+  DAT <- SetAllIdent(DAT, id = meta_var)
+  DAT <- StashIdent(DAT, save.name = meta_var)  
+  DEGs <- FindMarkers(DAT, ident.1=group1, ident.2=group2, test.use=test.use, only.pos = F)
+  DEGs <- replace_zero_pvals(DEGs)
+  DEGs$gene <- row.names(DEGs)
+  
+  cap <- paste("DEGs:\n",group1, "vs.", group2)
+  if(show.table==T){
+    createDT_html(DEGs, caption = cap)
+  }else{print("Not showing table")}
+  if(show.volcano==T){ volcanoPlot(DEGs, caption = cap)
+  }else(print("Not showing volcano plot")) 
+  
+  DAT <- SetAllIdent(DAT, id = "post_clustering")
+  return(DEGs)
+}
+
+DGE_within_clusters <- function(DAT, meta_var, group1, group2, clusterList, allClusts=F){ 
+  if(allClusts==T){ clusterList <- unique(DAT@meta.data$post_clustering) }
+  for (clust in clusterList){ 
+    cat('\n')   
+    cat("### ",paste("Cluster ",clust,": ",group1," vs. ", group2, sep="") , "\n")
+    DAT_clustSub <- Seurat::SubsetData(DAT, subset.name ="post_clustering", accept.value = clust, subset.raw = T)  
+    DEG_df <-runDGE(DAT_clustSub, meta_var, group1, group2 ) 
+    cat('\n')   
+  } 
 }
